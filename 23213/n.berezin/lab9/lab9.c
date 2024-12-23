3667b26587f63c31bfc7385b5f46f8549131f0e5
Nikita Berezin <n.berezin@g.nsu.ru>
2024-12-23 16:39:03 +0700

    Lab30 for tests

--- 23213/n.berezin/lab9/lab9.c
+++ 23213/n.berezin/lab9/lab9.c
@@ -5,21 +5,21 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]){
-    if (argc != 2) {
-        if (argc > 0) {
-            printf("Usage: %s <file>\n", argv[0]);
-        } else {
-            printf("Usage: lab9 <file>\n");
-        }
-        return 1;
-    }
+    // if (argc != 2) {
+    //     if (argc > 0) {
+    //         printf("Usage: %s <file>\n", argv[0]);
+    //     } else {
+    //         printf("Usage: lab9 <file>\n");
+    //     }
+    //     return 1;
+    // }
 
     pid_t pid = fork();
     if (pid == -1){
         perror("Can't fork");
         return 1;
     } else if (pid == 0){
-        execlp("cat", "cat", argv[1], NULL);
+        execlp("#!/home/vilmo/Documents/lrn/a.out hm", "cat", "hahha", "-oeau", NULL);
         perror("Can't exec cat");
         return 1;
     } else {
